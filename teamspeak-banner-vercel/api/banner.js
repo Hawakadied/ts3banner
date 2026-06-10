f429134d2c4f0ef321045cfbb1d04f7ab02ac9c4
Hawakadied <50178761+Hawakadied@users.noreply.github.com>
2026-06-10 22:00:02 +0200

    Update banner.js

--- teamspeak-banner-vercel/api/banner.js
+++ teamspeak-banner-vercel/api/banner.js
@@ -29,7 +29,7 @@ function formatBerlinTime() {
 
 function safeBackgroundUrl(request) {
   const { searchParams, origin } = new URL(request.url);
-  const image = searchParams.get('bg') || `${origin}/background.png`;
+  const image = searchParams.get('bg') || `${origin}/hf.png`;
 
   if (image.startsWith('http://') || image.startsWith('https://')) {
     return image;
